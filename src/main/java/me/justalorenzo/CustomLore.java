f1617b57c95e87be7622f173c7c8ad16c38debdf
TheRealBeaver <36443552+TheRealBeaver@users.noreply.github.com>
2021-08-10 19:38:18 +0200

    init commit

---
+++ src/main/java/me/justalorenzo/CustomLore.java
@@ -0,0 +1,10 @@
+package me.justalorenzo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CustomLore extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
